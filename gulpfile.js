ab93c6990ad6a54479360c80c30293d1a42ad59e
aryaziai <arya721@gmail.com>
2020-07-20 15:09:08 -0700

    copy html files to public

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ const gulp = require("gulp");
 gulp.task("message", async function () {
   return console.log("Gulp is running");
 });
+
+gulp.task("copyHtml", async function () {
+  gulp.src("src/*.html").pipe(gulp.dest("public"));
+});
