066912d19cec9102c6039cc8e946e915520357d9
aryaziai <arya721@gmail.com>
2020-07-20 15:49:22 -0700

    optimize images function

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const imagemin = require('gulp-imagemin');
 
 gulp.task("message", async function () {
   return console.log("Gulp is running");
@@ -7,3 +8,7 @@ gulp.task("message", async function () {
 gulp.task("copyHtml", async function () {
   gulp.src("src/*.html").pipe(gulp.dest("public"));
 });
+
+gulp.task("imageMin", () =>
+  gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("public/images"))
+);
